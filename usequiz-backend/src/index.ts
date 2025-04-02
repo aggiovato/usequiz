8afeb4dfb8a76afcf5ed8218fca0f2019ea2e2a6
Aggiovato <aggiovato@gmail.com>
2025-04-03 00:38:25 +0200

    feat: Set first questions to the DB and conn file

--- usequiz-backend/src/index.ts
+++ usequiz-backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import questionRoutes from "./routes/questionRoutes";
@@ -10,9 +11,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "static")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/home.html"));
+});
+
 app.use("/api/questions", questionRoutes);
 
-// 👇 conectamos la base de datos y arrancamos el servidor
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
--- usequiz-backend/src/models/Question.ts
+++ usequiz-backend/src/models/Question.ts
@@ -1,23 +1,39 @@
 import mongoose, { Schema } from "mongoose";
 
-const optionSchema = new Schema({
-  id: { type: String, required: true },
-  text: { type: String, required: true },
-});
-
-const questionSchema = new Schema({
-  subject: { type: String, required: true },
-  unit: { type: String, required: true },
-  question: { type: String, required: true },
-  options: [optionSchema],
-  answers: [{ type: String, required: true }],
-  explanation: String,
-  tags: [String],
-  difficulty: {
-    type: String,
-    enum: ["easy", "medium", "hard"],
-    default: "medium",
+const optionSchema = new Schema(
+  {
+    id: { type: String, required: true },
+    text: { type: String, required: true },
   },
-});
+  { _id: false }
+);
+
+const questionSchema = new Schema(
+  {
+    subject: { type: String, required: true },
+    unit: { type: String, required: true },
+    question: { type: String, required: true },
+    options: [optionSchema],
+    answers: [{ type: String, required: true }],
+    explanation: String,
+    tags: [String],
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      default: "medium",
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 export default mongoose.model("Question", questionSchema);
