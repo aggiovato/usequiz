0301deaf384d56f9cc4cb23e186d9e0f276b67c6
Aggiovato <aggiovato@gmail.com>
2025-04-06 19:40:37 +0200

    feat: Add style for 404 page, and redirect

--- usequiz-frontend/src/pages/404.tsx
+++ usequiz-frontend/src/pages/404.tsx
@@ -1,5 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import NotFoundIcon from "../components/icons/NotFoundIcon";
+import HomeIcon from "../components/icons/HomeIcon";
+import PaperplaneIcon from "../components/icons/PaperplaneIcon";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <NotFoundIcon className="w-120 h-60" />
+      <h1 className="text-2xl font-bold mt-8 mb-2">Ooooops! Are you lost?</h1>
+      <p className="text-sm mt-2">
+        It seems like the page you are looking for does not exist.
+      </p>
+      <p className="text-sm mt-2">
+        If you think this is an error, please contact us.
+      </p>
+
+      <div className="flex gap-4 mt-6">
+        <button
+          className="btn flex items-center justify-center gap-4"
+          onClick={() => navigate("/")}
+        >
+          Go home
+          <HomeIcon className="w-4 h-4 mb-0.5" />
+        </button>
+
+        <button
+          className="btn flex items-center justify-center gap-4"
+          onClick={() => navigate("/contact")}
+        >
+          Contact us
+          <PaperplaneIcon className="w-4 h-4" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
